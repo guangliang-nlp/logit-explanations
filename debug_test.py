a54506225337589c44eb030919e606f4a96fb074
Guangliang Liu <liuguangliang89@gmail.com>
2024-04-11 17:04:54 -0400

    Update debug_test.py

--- debug_test.py
+++ debug_test.py
@@ -1,11 +1,10 @@
-from transformers import (
-    AutoModelForMaskedLM,
-    AutoTokenizer,
-    AdamW,
-    DataCollatorForLanguageModeling,
-    AutoConfig
-)
 
-model = AutoModelForMaskedLM.from_pretrained("gpt2-xl")
+
+from transformers import GPT2Tokenizer, GPT2Model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
+model = GPT2Model.from_pretrained('gpt2-xl')
 for name, param in model.named_parameters():
     print(name)
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
