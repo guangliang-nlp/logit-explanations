d972941e9cfd0c9d7b7d80d50b618bee161db58f
Guangliang Liu <liuguangliang89@gmail.com>
2024-04-11 17:08:33 -0400

    Update debug_test.py

--- debug_test.py
+++ debug_test.py
@@ -1,8 +1,14 @@
 
-
+from transformers import LlamaTokenizer, LlamaForCausalLM
 from transformers import GPT2Tokenizer, GPT2Model
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
-model = GPT2Model.from_pretrained('gpt2-xl')
+name_path = "/scratch0/liuguan5/llama/llama-2-7b-chat-hf/"
+tokenizer = LlamaTokenizer.from_pretrained(name_path)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
+
+model = LlamaForCausalLM.from_pretrained(name_path, device_map='auto', output_attentions=True,
+                                             return_dict=True)
+
 for name, param in model.named_parameters():
     print(name)
 text = "Replace me by any text you'd like."
